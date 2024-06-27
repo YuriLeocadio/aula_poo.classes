a3c3cdd363c621d6a21752183f3288cbedf892cd
Yuri Moreira Leocadio <yurileocadio0411@gmail.com>
2024-06-27 20:54:19 -0300

    first commit

---
+++ src/test/java/com/poo/classes/ClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.classes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
