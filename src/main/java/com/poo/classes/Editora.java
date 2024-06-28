3a80c8e79d84eb02a7a961796742782c916446a1
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-06-28 23:39:22 +0000

    Adição de getters e setter na classe livro

--- src/main/java/com/poo/classes/Editora.java
+++ src/main/java/com/poo/classes/Editora.java
@@ -1,7 +1,24 @@
 package com.poo.classes;
 
 public class Editora {
-    int editora_id;
-    String nome;
+    private int editora_id;
+    private String nome;
+
+    public int getEditora_id() {
+        return editora_id;
+    }
+
+    public void setEditora_id(int editora_id) {
+        this.editora_id = editora_id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
+
 
