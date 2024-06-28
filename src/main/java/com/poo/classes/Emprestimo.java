4a3bf6f00a833e2cc5ee34643c78d48b0b2eb93f
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-06-28 01:00:57 +0000

    Criação da classe emprestimo

---
+++ src/main/java/com/poo/classes/Emprestimo.java
@@ -0,0 +1,11 @@
+package com.poo.classes;
+
+import java.time.LocalDate;
+
+public class Emprestimo {
+    int emprestimo_id;
+    LocalDate data_emprestimo;
+    LocalDate data_prev_dev;
+    int pessoa_id;
+}
+
