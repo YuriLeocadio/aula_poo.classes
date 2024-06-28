e8fd20313e3e9d6cbcdaf107f50b816448e5632b
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-06-28 23:59:06 +0000

    Adição de getters e setter na classe exemplar

--- src/main/java/com/poo/classes/Exemplar.java
+++ src/main/java/com/poo/classes/Exemplar.java
@@ -1,7 +1,24 @@
 package com.poo.classes;
 
 public class Exemplar {
-    int exemplar_id;
-    int livro_id;
+    private int exemplar_id;
+    private int livro_id;
+
+    public int getExemplar_id() {
+        return exemplar_id;
+    }
+
+    public void setExemplar_id(int exemplar_id) {
+        this.exemplar_id = exemplar_id;
+    }
+
+    public int getLivro_id() {
+        return livro_id;
+    }
+
+    public void setLivro_id(int livro_id) {
+        this.livro_id = livro_id;
+    }
 }
+
 
