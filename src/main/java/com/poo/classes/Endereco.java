a3a776b4bb2814ca9e1e1044980e25c28e4f479c
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-06-29 00:31:29 +0000

    Adição de getters e setter na classe endereço

--- src/main/java/com/poo/classes/Endereco.java
+++ src/main/java/com/poo/classes/Endereco.java
@@ -1,10 +1,59 @@
 package com.poo.classes;
 
 public class Endereco {
-    String uf;
-    String cidade;
-    String tipo;
-    String logradouro;
-    String complemento;
-    int endereco_id;
+    private String uf;
+    private String cidade;
+    private String tipo;
+    private String logradouro;
+    private String complemento;
+    private int endereco_id;
+
+    public String getuf() {
+        return uf;
+    }
+
+    public void setuf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getcidade() {
+        return cidade;
+    }
+
+    public void setcidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String gettipo() {
+        return tipo;
+    }
+
+    public void settipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getlogradouro() {
+        return logradouro;
+    }
+
+    public void setlogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getcomplemento() {
+        return complemento;
+    }
+
+    public void setcomplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public int getendereco_id() {
+        return endereco_id;
+    }
+
+    public void setendereco_id(int endereco_id) {
+        this.endereco_id = endereco_id;
+    }
 }
+
--- src/main/java/com/poo/classes/Pessoa.java
+++ src/main/java/com/poo/classes/Pessoa.java
@@ -1,10 +1,51 @@
 package com.poo.classes;
 
 public class Pessoa {
-    int pessoa_id;
-    String rg;
-    String cpf;
-    String nome;
-    int endereco_id;
+    private int pessoa_id;
+    private String rg;
+    private String cpf;
+    private String nome;
+    private int endereco_id;
+
+    public int getPessoa_id() {
+        return pessoa_id;
+    }
+
+    public void setPessoa_id(int pessoa_id) {
+        this.pessoa_id = pessoa_id;
+    }
+
+    public String getrg() {
+        return rg;
+    }
+
+    public void setrg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getcpf() {
+        return cpf;
+    }
+
+    public void setcpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getnome() {
+        return nome;
+    }
+
+    public void setnome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getendereco_id() {
+        return endereco_id;
+    }
+
+    public void setendereco_id(int endereco_id) {
+        this.endereco_id = endereco_id;
+    }
 }
+
 
