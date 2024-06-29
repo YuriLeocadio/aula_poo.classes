b878309c2359b21d0aadbfd4a9eae83643cca180
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-06-29 00:21:47 +0000

    Adição de getters e setter na classe Exedevolução

--- src/main/java/com/poo/classes/Devolucao.java
+++ src/main/java/com/poo/classes/Devolucao.java
@@ -3,7 +3,23 @@ package com.poo.classes;
 import java.time.LocalDate;
 
 public class Devolucao {
-    LocalDate data_devolvido;
-    int devolucao_id;
+    private LocalDate data_devolvido = LocalDate.now();
+    private int devolucao_id;
+
+    public LocalDate getData_devolvido() {
+        return data_devolvido;
+    }
+
+    public void setData_devolvido(LocalDate data_devolvido) {
+        this.data_devolvido = data_devolvido;
+    }
+
+    public int getDevolucao_id() {
+        return devolucao_id;
+    }
+
+    public void setDevolucao_id(int devolucao_id) {
+        this.devolucao_id = devolucao_id;
+    }
 }
 
