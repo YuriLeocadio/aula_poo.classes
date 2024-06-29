914f30d13870970e1bc98917d645cdbb1db0aa86
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-06-29 00:13:14 +0000

    Adição de getters e setter na classe ExemplarEmprestimo

--- src/main/java/com/poo/classes/ExemplarEmprestimo.java
+++ src/main/java/com/poo/classes/ExemplarEmprestimo.java
@@ -1,8 +1,41 @@
 package com.poo.classes;
 
 public class ExemplarEmprestimo {
-    int exemplar_emprestimo_id;
-    int exemplar_id;
-    int emprestimo_id;
-    int devolucao_id;
+    private int exemplar_emprestimo_id;
+    private int exemplar_id;
+    private int emprestimo_id;
+    private int devolucao_id;
+
+    public int getExemplar_emprestimo_id() {
+        return exemplar_emprestimo_id;
+    }
+
+    public void setExemplar_emprestimo_id(int exemplar_emprestimo_id) {
+        this.exemplar_emprestimo_id = exemplar_emprestimo_id;
+    }
+
+    public int getExemplar_id() {
+        return exemplar_id;
+    }
+
+    public void setExemplar_id(int exemplar_id) {
+        this.exemplar_id = exemplar_id;
+    }
+
+    public int getEmprestimo_id() {
+        return emprestimo_id;
+    }
+
+    public void setEmprestimo_id(int emprestimo_id) {
+        this.emprestimo_id = emprestimo_id;
+    }
+
+    public int getDevolucao_id() {
+        return devolucao_id;
+    }
+
+    public void setDevolucao_id(int devolucao_id) {
+        this.devolucao_id = devolucao_id;
+    }
 }
+
